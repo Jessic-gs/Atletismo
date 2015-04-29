1c37a0eb120c2423e3cdee424c0f59ad7067e2e3
Felipe Willian Torres de Camargo <felipetorresdecamargo@hotmail.com>
2015-04-29 18:43:41 -0300

    novo

--- src/br/atletismo/dominio/Atleta.java
+++ src/br/atletismo/dominio/Atleta.java
@@ -10,6 +10,25 @@ public class Atleta {
 	private int id;
 	private String nome;
 	private String sexo;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
 
 
 }
